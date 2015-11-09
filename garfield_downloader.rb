c8c9ff85c8cf7d4f7158ef95dd53ef2de468dd14
Kyle Tolle <kyle@nullsix.com>
2015-11-08 19:02:30 -0700

    Handle the earliest comit strip date more nicely

--- garfield_downloader.rb
+++ garfield_downloader.rb
@@ -38,13 +38,20 @@ class Garfield
         end
 
 
-        current_date -= 1
+        current_date = current_date.prev_day
 
         print_progress
+
+        if current_date < earliest_comic_strip_date
+          print "\rRetrieved all the Garfield comic strips, from today back to the first one!\n"
+          break
+        end
       end
     rescue OpenURI::HTTPError
-      puts "Couldn't open #{current_image_url}..."
+      puts "Couldn't find a comic strip for #{current_date}."
+
     rescue Interrupt
+    ensure
       print "\r"
       print_progress
       print "\n"
@@ -86,6 +93,11 @@ class Garfield
 
     def print_progress
       print "Total: #{total_images_count}; Downloaded: #{downloaded_images_count}; Skipped: #{skipped_images_count}\r"
+    end
+
+    def earliest_comic_strip_date
+      # Source for earliest date: https://en.wikipedia.org/wiki/Garfield
+      Date.parse('June 19, 1978')
     end
   end
 end
